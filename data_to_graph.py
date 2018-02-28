8407958e80abfdccee26f12b295f4859ad2a5b2d
David <dmunoz5@ucsc.edu>
2018-02-27 17:53:11 -0800

    Script for Graphing Data: Update

--- data_to_graph.py
+++ data_to_graph.py
@@ -6,14 +6,15 @@ import matplotlib.patches as mpatches
 text_file = sys.argv[1]
 data_entry = False
 end_flag = False
-test_type = []
+test_type = ''
 source = []
 similarity = []
 camera_software = []
 distance = []
 best_value = []
+plate = ''
 
-def dograph(value,title,source,datax):
+def dograph(value,title,source,datax,plate):
     # Stripping New Line and Converting String into INT to use in MatPlot
     for i in range(len(value)):
         value[i].rstrip('\n')
@@ -25,16 +26,20 @@ def dograph(value,title,source,datax):
 
     plt.xlabel('Distance')
     plt.ylabel('Plate Similarity')
-    plt.title(title[0])
+
+    new_title  = title + ' ' + plate
+    plt.title(new_title)
+
     # Splitting Data between Both sources
     even = best_value[0::2]
     odd = best_value[1::2]
 
-    plt.plot([datax],[even], color='red', linestyle='-', marker='o')
-    plt.plot([datax],[odd],color='green', linestyle='-', marker = 'o')
+    plt.plot(datax,even, color='red', linestyle='-', marker='o')
+
+    plt.plot(datax,odd,color='green', linestyle='-', marker = 'o')
+
     red_patch = mpatches.Patch(color='red', label='OpenALPR')
     green_patch = mpatches.Patch(color='green', label='Cloud-OpenALPR')
-    
     plt.legend(handles=[red_patch,green_patch])
 
     plt.savefig(title[0])
@@ -47,14 +52,26 @@ parse = open(text_file,'r')
 for line in parse:
     data = line.split(':::')
     for i,j in enumerate(data):
+        if j == "Plate":
+            plate = data[i+1]
         if j == "Test":
             if data_entry:
                 data_entry = False
                 # Creates Graph once it encounters a different test
-                if test_type[0] != data[i+1]:
-                    dograph(best_value,test_type,source,distance)
+                if test_type != data[i+1]:
+                    dograph(best_value,test_type,source,distance,plate)
+                    #Reset Variables
+                    text_file = sys.argv[1]
+                    data_entry = False
+                    end_flag = False
+                    test_type = ''
+                    source = []
+                    similarity = []
+                    camera_software = []
+                    distance = []
+                    best_value = []
             data_entry = True
-            test_type.append(data[i+1])
+            test_type = data[i+1]
             distance.append(data[i+2])
         if j == "Source":
             source.append(data[i+1])
